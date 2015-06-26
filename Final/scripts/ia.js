9f0ddacbfed6ce7fc4e0283bdd255600c37f3ec0
Lionel Paulus <hello@lionelpaul.us>
2015-06-26 07:26:13 +0200

    Mise à jour inté

--- Final/scripts/ia.js
+++ Final/scripts/ia.js
@@ -1,18 +1,49 @@
 function ia() {
-  var x ;
-  var y;
-  /*for(k = 0 ; k < 9 ; k++){
-    for(l = 0; l < 9 ; l++){
-
+  var count = 0;
+  for (x = 0; x < rows; x++) {
+    for (y = 0; y < rows; y++) {
+      if (atari(x, y) == true) {
+        if (attributePos(x, y) != "nope") {
+          console.log("hehe");
+          console.log(attributePos(x, y));
+          return attributePos(x, y);
+        }
+      }
     }
-  }*/
-  var stratégie = Math.floor
-  x = Math.floor((Math.random() * 9));
-  y = Math.floor((Math.random() * 9));
-  if(suicide(x,y) == false && ko(x,y) == false){
-      return (x + "_" + y);
   }
-  else {
-    ia();
+
+  do {
+    var x = (Math.floor(Math.random() * rows));
+    var y = (Math.floor(Math.random() * rows));
+  } while(possible(x, y) == false && count < 42);
+  if(count == 41){
+    game_finished = true;
+  }
+  return (x + "_" + y);
+
+}
+
+function possible(x, y){
+  if ((x < rows) && (x >= 0) && (y < rows) && (y >= 0)) {
+    if (grid[x][y] == 0) {
+      if ((suicide(x, y) == false) && (ko(x, y) == false)) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function attributePos(x, y) {
+  if ((x + 1) < rows && possible(x + 1, y) == true) {
+    return ((x + 1) + "_" + y);
+  } else if ((x - 1) >= 0 && possible(x - 1, y) == true) {
+    return ((x - 1) + "_" + y);
+  } else if ((y + 1) < rows && possible(x, y + 1) == true) {
+    return (x + "_" + (y + 1));
+  } else if ((y - 1) >= 0 && possible(x, y - 1) == true) {
+    return (x + "_" + (y - 1));
+  } else {
+    return ("nope");
   }
 }
--- Final/scripts/main.js
+++ Final/scripts/main.js
@@ -1,15 +1,26 @@
 //// Variables
 var tour = 0;
 var rows = 9;
-var score_one = 0;
-var score_two = 7.5;
 var player = 1;
 var game_history = [];
 var game_finished = false;
 var last_skip = 0; // Used for the end of the game
-var ia_mode = false;
+if(window.location.search == "?mode=duo"){
+  var ia_mode = true;
+}else {
+  var ia_mode = false;
+}
 
 //// We declare the array's
+// score array
+var score = [];
+score[1] = [];
+score[2] = [];
+score[1]["territory"] = 0;
+score[1]["present_paws"] = 0;
+score[2]["territory"] = 0;
+score[2]["present_paws"] = 7.5;
+
 // group array -> contain the ID of the group for each row
 var group = new Array();
 for (var i = 0; i < rows; i++) {
@@ -33,16 +44,11 @@ generate_cells();
 function generate_background() {
   var background = '<table id="tableau"><tbody>';
   for (var a = 1; a < rows; a++) {
-
     background += '<tr>';
-
     for (var b = 1; b < rows; b++) {
-
       background += '<td class="cellule"></td>';
     }
-
     background += '</tr>';
-
   }
   document.getElementById("goban").innerHTML = '</tbody></table>' + background;
 }
@@ -50,23 +56,15 @@ function generate_background() {
 // Clickable DIV
 function generate_cells() {
   var goban = '';
-
   document.getElementById("grid").innerHTML = "";
-
   for (a = 0; a < rows; a++) {
     goban += "<div class='lines'>";
-
     for (b = 0; b < rows; b++) {
-
       goban += '<div id="' + (a + "_" + b) + '" class="empty" onclick="next_step(id);"></div>';
     }
-
     goban = goban + '</div>';
-
   }
-
   document.getElementById("grid").innerHTML = goban;
-
 }
 
 //// Onclick of the DIV
@@ -99,11 +97,13 @@ function next_step(id) {
       return;
     } else {
       if (ko(x, y) == false) {
+        grid[x][y] = player;
         identify_groups();
         scores();
         capture(x, y);
         update_html();
         tour += 1;
+        auto_backup();
 
         atari(x, y);
         if (x + 1 <= 8) {
@@ -119,16 +119,20 @@ function next_step(id) {
           atari(x, y - 1);
         }
 
-        if (player == 1 && ia_mode == true) {
-          var ia_data = ia();
-          next_step(ia_data);
-        }
-
-        // Player alternation
+        // Player alternation and score + 1
         if (player == 1) {
           player = 2;
         } else {
           player = 1;
+        }
+
+        // IA turn
+        if (player == 2 && ia_mode == true) {
+          //setTimeout(function() {
+          var ia_data = ia();
+          console.log("Position IA: " + ia_data);
+          next_step(ia_data);
+          //}, 4000);
         }
       }
     }
@@ -138,11 +142,32 @@ function next_step(id) {
 // Detect if player is trying to commit suicide or not
 function suicide(x, y) {
   grid[x][y] = player;
-  if (liberties(x, y) == 0 && (liberties(x + 1, y) != 0 && liberties(x - 1, y) != 0 && liberties(x, y + 1) != 0 && liberties(x, y - 1) != 0)) {
+  console.log("player: "+ player);
+  var dead = false;
+
+  /*if (liberties(x, y) == 0 && (liberties(x + 1, y) != 0 && liberties(x - 1, y) != 0 && liberties(x, y + 1) != 0 && liberties(x, y - 1) != 0)) {
     grid[x][y] = 0;
     return true;
+  }*/
+
+  if (liberties(x, y) == 0) {
+    dead = true;
+    if ((y - 1) >= 0 && liberties(x, y - 1) == 0 && grid[x][y - 1] != player) {
+      dead = false;
+    } else if ((x + 1) < rows && liberties(x + 1, y) == 0 && grid[x + 1][y] != player) {
+      dead = false;
+    } else if ((y + 1) < rows && liberties(x, y + 1) == 0 && grid[x][y + 1] != player) {
+      dead = false;
+    } else if ((x - 1) >= 0 && liberties(x - 1, y) == 0 && grid[x - 1][y] != player) {
+      dead = false;
+    }
   }
-  return false;
+
+  if (dead == true) {
+    grid[x][y] = 0;
+    return true; // Suicide interdit
+  }
+  return false; // Suicide autorisé ou pas de suicide
 }
 
 function identify_groups() {
@@ -254,6 +279,11 @@ function count_liberties(x, y) {
   }
   if (captured_cells > 0) {
     console.log("Le joueur " + player + " a capturé " + captured_cells + " cases à l'ennemi !");
+    if (player == 1) {
+      score[2]["present_paws"] -= captured_cells;
+    } else {
+      score[1]["present_paws"] -= captured_cells;
+    }
   }
   captured_cells = 0;
 }
@@ -279,27 +309,40 @@ function update_html() {
   }
 }
 
-function save_game() {
-  var game_state = JSON.stringify(grid);
-  localStorage.setItem('game_state', game_state);
-}
-
-function reload_game() {
-  if (localStorage.getItem('game_state') == null) {
-    window.alert("Aucun état de jeu sauvegardé !");
-  } else {
-    grid = JSON.parse(localStorage.getItem('game_state'));
-    update_html();
+function auto_backup() {
+  if (localStorage.getItem('game_state') != null && tour == 0) {
+    swal({
+        title: "Hé on se connaît non ?",
+        text: "Vous n'aviez pas terminé votre dernière partie, souhaitez-vous la reprendre ?",
+        imageUrl: "images/backup.png",
+        confirmButtonText: "Reprendre la partie",
+        showCancelButton: true,
+        cancelButtonText: "Nouvelle partie"
+      },
+      function() {
+        grid = JSON.parse(localStorage.getItem('game_state'));
+        score[1]["present_paws"] = localStorage.getItem('score_1_present_paws');
+        score[1]["territory"] = localStorage.getItem('score_1_territory');
+        score[2]["present_paws"] = localStorage.getItem('score_2_present_paws');
+        score[2]["territory"] = localStorage.getItem('score_2_territory');
+        document.getElementById("score_one").innerHTML = score[1]["present_paws"] + score[1]["territory"];
+        document.getElementById("score_two").innerHTML = score[2]["present_paws"] + score[2]["territory"];
+        update_html();
+      });
+  }
+  if (tour != 0) {
+    var game_state = JSON.stringify(grid);
+    localStorage.setItem('game_state', game_state);
+    localStorage.setItem('score_1_present_paws', score[1]["present_paws"]);
+    localStorage.setItem('score_1_territory', score[1]["territory"]);
+    localStorage.setItem('score_2_present_paws', score[2]["present_paws"]);
+    localStorage.setItem('score_2_territory', score[2]["territory"]);
   }
 }
 
 function liberties(x, y) {
   identify_groups();
-
   var group_id = group[x][y];
-  if (group_id == 0) {
-    return "Aucun pion à cet endroit.";
-  }
   var liberte = 0;
   var liberties_already_counted = [];
 
@@ -343,7 +386,10 @@ function inArray(tableau, element) {
 
 function atari(x, y) {
   if (liberties(x, y) == 1) {
-    window.alert("ATARI en " + x + "/" + y);
+    console.log(("ATARI en " + x + "/" + y));
+    return true;
+  } else {
+    return false;
   }
 }
 
@@ -361,6 +407,7 @@ function ko(x, y) {
       grid[x][y] = 0;
       return true;
     }
+    grid[x][y] = 0;
     return false;
   }
 }
@@ -424,8 +471,8 @@ function skip(spec) {
 function scores() {
   if (tour > 1) {
     var scores_by_group = [];
-    score_one = 0;
-    score_two = 0;
+    score[1]["territory"] = 0;
+    score[2]["territory"] = 0;
     for (x = 0; x < rows; x++) {
       for (y = 0; y < rows; y++) {
         var group_id = group[x][y];
@@ -476,15 +523,38 @@ function scores() {
 
     // Calculate the totals
     for (var i = 0; i < scores_by_group.length; i++) {
-      if (scores_by_group[i] && scores_by_group[i]["neutral"] == false) {
+      if ((typeof(scores_by_group[i]) != "undefined") && (scores_by_group[i]["neutral"] == false)) {
         if (scores_by_group[i]["player"] == 1) {
-          score_one += scores_by_group[i]["points"];
+          score[1]["territory"] += scores_by_group[i]["points"];
         } else {
-          score_two += scores_by_group[i]["points"];
+          score[2]["territory"] += scores_by_group[i]["points"];
         }
       }
     }
-    document.getElementById("score_one").innerHTML = score_one;
-    document.getElementById("score_two").innerHTML = score_two;
+  }
+
+  // Player score + 1
+  if (player == 1) {
+    score[1]["present_paws"] += 1;
+  } else {
+    score[2]["present_paws"] += 1;
+  }
+
+
+  document.getElementById("score_one").innerHTML = (score[1]["territory"] + score[1]["present_paws"]);
+  document.getElementById("score_two").innerHTML = (score[2]["territory"] + score[2]["present_paws"]);
+
+
+  if (tour > 15 && ia_mode == true) {
+    game_finished = true;
+    for (var k = 0; k < scores_by_group.length; k++) {
+      if ((typeof(scores_by_group[k]) != "undefined") && (scores_by_group[k]["neutral"] == true)) {
+        game_finished = false;
+      }
+    }
   }
 }
+
+
+/// Start elementary fonctions
+//auto_backup();
